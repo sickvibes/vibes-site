ad6de16f04681396cac237546fd9a44a6e1be7f5
Brandon Valosek <bvalosek@gmail.com>
2021-08-22 13:50:32 -0500

    use static rpc provider

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,14 +1,13 @@
 import { providers } from 'ethers';
-import { JsonRpcProvider } from '@ethersproject/providers';
+import { StaticJsonRpcProvider } from '@ethersproject/providers';
 
-export const getProvider = (): JsonRpcProvider => {
-  // return new providers.JsonRpcProvider('https://polygon-mainnet.infura.io/v3/6a5ddbf0cff0460eb7931664d7495ef9', 137);
-  return new providers.JsonRpcProvider(
+export const getProvider = (): StaticJsonRpcProvider => {
+  return new providers.StaticJsonRpcProvider(
     'https://polygon-mainnet.g.alchemy.com/v2/jr_W_XO155rJiT34TSnq9iwA0fe5N231',
     137
   );
 };
 
-export const getEthProvider = (): JsonRpcProvider => {
-  return new providers.JsonRpcProvider('https://mainnet.infura.io/v3/6a5ddbf0cff0460eb7931664d7495ef9', 1);
+export const getEthProvider = (): StaticJsonRpcProvider => {
+  return new providers.StaticJsonRpcProvider('https://mainnet.infura.io/v3/6a5ddbf0cff0460eb7931664d7495ef9', 1);
 };
