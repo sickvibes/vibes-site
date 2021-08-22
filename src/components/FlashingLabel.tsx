d0134c038c5324dd7d53d74807847e063bb79abf
Brandon Valosek <bvalosek@gmail.com>
2021-08-22 14:57:19 -0500

    flashing label accent

--- src/components/FlashingLabel.tsx
+++ src/components/FlashingLabel.tsx
@@ -4,26 +4,27 @@ import { ThemeConfig } from '../Theme';
 
 interface Props {
   label?: string;
+  accent?: boolean;
 }
 
-const useStyles = makeStyles<ThemeConfig>((theme) => {
+const useStyles = makeStyles<ThemeConfig, Props>((theme) => {
   return {
     notFlash: {
-      color: theme.palette.accent.quadriarylolwhat,
+      color: (props) => (props.accent ? theme.palette.accent.main : theme.palette.accent.quadriarylolwhat),
     },
     flash: {
-      color: theme.palette.accent.tertiary,
+      color: (props) => (props.accent ? 'white' : theme.palette.accent.tertiary),
     },
   };
 });
 
 export const FlashingLabel: FunctionComponent<Props> = (props) => {
-  const classes = useStyles();
+  const classes = useStyles(props);
   const flash = useRef(0);
   const [isFlash, setIsFlash] = useState(false);
   const flashStart = 5;
   const flashEnd = 7;
-  const clickTime = 100;
+  const clickTime = props.accent ? 50 : 100;
 
   useEffect(() => {
     const h = setInterval(() => {
